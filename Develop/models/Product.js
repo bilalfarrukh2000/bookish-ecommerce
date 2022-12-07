65a2433260f2eaa21d4e2a49ebfc9fde92ea21ef
bilalfarrukh2000 <bilalfarrukh2000@gmail.com>
2022-12-06 17:02:11 -0800

    Add Product Model

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -10,6 +10,36 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
+        id: {
+          type: DataTypes.INTEGER,
+          allowNull: false,
+          primaryKey: true,
+          autoIncrement: true,
+        },
+        price: {
+          type: DataTypes.DECIMAL,
+          allowNull: false,
+          validate: {isDecimal:true},
+        },
+        
+        stock: {
+          type: DataTypes.INTEGER,
+          allowNull: false,
+          validate: {isNumeric:true},
+        },
+            
+        product_name: {
+          type: DataTypes.STRING,
+          allowNull: false,
+        },
+    
+        category_id: {
+          type: DataTypes.INTEGER,
+          references: {
+            model: "Category",
+            key: "id"
+          }
+        }
   },
   {
     sequelize,
